06262d99fc9fb931b91297eeb8a4c027832d06ef
Mikhail Gunin <gunka462@gmail.com>
2024-08-30 19:33:05 +0300

    Add `--default`

--- install.sh
+++ install.sh
@@ -2,12 +2,24 @@
 
 set -eo pipefail
 
+USR_LOCAL_ETC=/usr/local/etc
+
+DEFAULT_TRAFFIC_DATA_DIR=$USR_LOCAL_ETC/traffic-data
+DEFAULT_USERS_JSON_PATH=$USR_LOCAL_ETC/xray-stats-telegram/users.json
+
 trafficDataDir=$1
 botToken=$2
 usersJsonPath=$3
 
+if [ "$1" == "--default" ]; then
+    trafficDataDir=$DEFAULT_TRAFFIC_DATA_DIR
+    usersJsonPath=$DEFAULT_USERS_JSON_PATH
+fi
+
 if [ -z "$trafficDataDir" ] || [ -z "$botToken" ] || [ -z "$usersJsonPath" ]; then
     echo "Usage: $0 <traffic-data-dir> <bot-token> <users-json-path>"
+    echo "Usage: $0 --default <bot-token>"
+    echo "(defaults to $DEFAULT_TRAFFIC_DATA_DIR and $DEFAULT_USERS_JSON_PATH)"
     exit 1
 fi
 
