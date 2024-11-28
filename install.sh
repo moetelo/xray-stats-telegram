4f42238daf5517038f1f60e951fef00599ee5c17
Mikhail Gunin <gunka462@gmail.com>
2024-11-28 14:55:13 +0300

    fix: allow `./install.sh` without updating bot token

--- install.sh
+++ install.sh
@@ -3,20 +3,17 @@
 set -eo pipefail
 
 botToken=$1
-if [ -z "$botToken" ]; then
-    echo "Usage: $0 <bot-token>"
-    exit 1
-fi
 
 go build -o /usr/local/bin/xray-stats-telegram
 
-sed "s|<bot-token>|$botToken|" xray-stats-telegram.service \
-    > /etc/systemd/system/xray-stats-telegram.service
+if [ -n "$botToken" ]; then
+    sed "s|<bot-token>|$botToken|" xray-stats-telegram.service \
+        > /etc/systemd/system/xray-stats-telegram.service
+fi
 
 mkdir -p /usr/local/etc/xray-stats-telegram
 touch /usr/local/etc/xray-stats-telegram/admins
 touch /usr/local/etc/xray-stats-telegram/users
 
-systemctl daemon-reload
 systemctl enable xray-stats-telegram
 systemctl restart xray-stats-telegram
