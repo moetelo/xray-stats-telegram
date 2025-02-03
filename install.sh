59a45b4334eb0535f8441dbd45573100664f10f1
Mikhail Gunin <gunka462@gmail.com>
2025-02-03 23:38:17 +0300

    fix install.sh

--- install.sh
+++ install.sh
@@ -3,17 +3,21 @@
 set -eo pipefail
 
 botToken=$1
-if [ -z "$botToken" ]; then
-    echo "Usage: $0 <bot-token>"
-    exit 1
-fi
 
 cargo build --release
 cp ./target/release/xray-stats-telegram-rs /usr/local/bin/xray-stats-telegram
 
-sed "s|<bot-token>|$botToken|" xray-stats-telegram.service \
-    > /etc/systemd/system/xray-stats-telegram.service
 
-systemctl daemon-reload
+if [ -n "$botToken" ]; then
+    sed "s|<bot-token>|$botToken|" xray-stats-telegram.service \
+        > /etc/systemd/system/xray-stats-telegram.service
+
+    systemctl daemon-reload
+fi
+
+mkdir -p /usr/local/etc/xray-stats-telegram
+touch /usr/local/etc/xray-stats-telegram/admins
+touch /usr/local/etc/xray-stats-telegram/users
+
 systemctl enable xray-stats-telegram
 systemctl restart xray-stats-telegram
